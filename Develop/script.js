ef0e262a16964939e830ef3cad84e1e449b42ecc
Brandon Piercy <b.piercy567@gmail.com>
2020-11-01 16:11:04 -0800

    added variables for all character types and code to convert items to strings

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var numbers = ["0123456789"];
+var specialChar = [" !#$%&'()*+,-./:;<=>?@[]\^_`{}|~"];
+var lowerSplit = lowerCase.toString().split("");
+var upperSplit = upperCase.toString().split("");
+var numSplit = numbers.toString().split("");
+var specialSplit = specialChar.toString().split("");
+
+console.log(lowerSplit);
+console.log(upperSplit);
+console.log(numSplit);
+console.log(specialSplit);
+
+
 
 // Write password to the #password input
 function writePassword() {
